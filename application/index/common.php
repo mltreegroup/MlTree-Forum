fd0cbf6a533760d510a9007003f7b87ed2637432
Kingsr <kingsrml@vip.qq.com>
2018-05-26 18:36:33 +0800

    修改了后台管理界面左侧菜单无法弹出的问题。
    Signed-off-by: Kingsr <kingsrml@vip.qq.com>

--- application/index/common.php
+++ application/index/common.php
@@ -6,7 +6,7 @@ function outBadge($data)
 {
     $value = '';
     if ($data['tops'] == 1) {
-        $value = ' <span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">置顶</span>';
+        $value = '<span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">置顶</span>';
     }
     if ($data['essence'] == 1) {
         $value = $value.'<span class="mf-badge mf-badge-warning mf-radius mdui-m-r-1">精华</span>';
@@ -22,4 +22,21 @@ function authCheck($name, $uid = 0, $relation = 'or')
         $uid = session('uid');
     }
     return $auth->check($name, $uid, $relation);
+}
+
+function outAdminMenu($uid = 0)
+{
+    $value = '';
+    if($uid == 0 || empty(session('uid')))
+    {
+        return;
+    }else if($uid == 0){
+        $uid = session('uid');
+    }
+    $auth = new Auth();
+    if($auth->check('admin',$uid))
+    {
+        $value = '<span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">封禁用户</span>';
+        $value += '<span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">删除帖子</span>';
+    }
 }

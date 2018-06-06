8092a148517e27d90ca5e87f942b70bb45859069
Kingsr <kingsrml@vip.qq.com>
2018-06-06 11:15:25 +0800

    临时提交

--- application/index/common.php
+++ application/index/common.php
@@ -23,20 +23,3 @@ function authCheck($name, $uid = 0, $relation = 'or')
     }
     return $auth->check($name, $uid, $relation);
 }
-
-function outAdminMenu($uid = 0)
-{
-    $value = '';
-    if($uid == 0 || empty(session('uid')))
-    {
-        return;
-    }else if($uid == 0){
-        $uid = session('uid');
-    }
-    $auth = new Auth();
-    if($auth->check('admin',$uid))
-    {
-        $value = '<span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">封禁用户</span>';
-        $value += '<span class="mf-badge mf-badge-danger mf-radius mdui-m-r-1">删除帖子</span>';
-    }
-}
